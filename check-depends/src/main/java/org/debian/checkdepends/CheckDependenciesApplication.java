514fad60c336454937132b84f1344d42cf971e5c
Vladimir Petko <vladimir.petko@canonical.com>
2025-06-16 16:45:28 +1200

    chore: generate check-depends

---
+++ check-depends/src/main/java/org/debian/checkdepends/CheckDependenciesApplication.java
@@ -0,0 +1,13 @@
+package org.debian.checkdepends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckDependenciesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckDependenciesApplication.class, args);
+	}
+
+}
---
+++ check-depends/src/test/java/org/debian/checkdepends/CheckDependenciesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.debian.checkdepends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckDependenciesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
